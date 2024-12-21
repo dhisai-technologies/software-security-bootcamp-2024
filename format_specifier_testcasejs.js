53cabd9f34b91674a75fbbb03b4b3b61dc776f93
snehanm4281 <130364725+snehanm4281@users.noreply.github.com>
2024-12-21 14:45:36 +0530

    Add files via upload
    Test cases for format_specifier question(input for the format_specifier _vulnerability scanner)

---
+++ format_specifier_testcasejs.js
@@ -0,0 +1,3 @@
+
+var user_input = prompt("Enter JavaScript code:");
+eval(user_input);  
---
+++ formatspecifier_testcase.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    char *name = "Alice";
+    printf(name); 
+    return 0;
+}
