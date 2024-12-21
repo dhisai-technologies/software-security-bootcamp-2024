f9faa6a9d1efc4489145cfd093a88a4c0fc0f8ef
mokshigithub <vtu21712@veltech.edu.in>
2024-12-21 15:44:47 +0530

    completed

---
+++ overflow_vulnerabilities_example.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    int a = 2147483647;
+    int b = 1;
+    
+    
+    int sum = a + b;  
+
+    
+    int product = a * b;  
+    
+    short small_val = (short)a; 
+
+    return 0;
+}
---
+++ shopping_details_vulnerable_example.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main() {
+    int item_price = 1000;
+    int quantity = 1000000;
+    
+    
+    int total_cost = item_price + quantity;  
+
+    int large_cost = item_price * quantity;  
+    
+    
+    short small_cost = (short)item_price;  
+
+    return 0;
+}
