2bd959dfedba0d78498613586980f5ca5a34d031
zuhairsk <zuhairsk2005@gmail.com>
2024-12-21 16:34:44 +0530

    Add IntegerOverFlows.cpp file

---
+++ IntegerOverFlows.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int x = INT_MAX;
+    x = x + 1;  
+
+    int y = INT_MIN;
+    y = y - 1;  
+
+    int a = -1;
+    unsigned int b = (unsigned int)a;  
+
+    return 0;
+}
